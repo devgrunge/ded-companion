c35dbecf03c3e3bd95951da1ab03c7911eca47d3
alexanderJr <alexmarcjr21@gmail.com>
2023-09-23 20:04:53 +0100

    feat: GET, POST, PUT and DELETE methods, with search by query parameteres

--- databaseMemory.js
+++ databaseMemory.js
@@ -1,11 +1,36 @@
-export class DatabaseMemory {
-  #videos = new Map();
+import { randomUUID } from "node:crypto";
 
-  create(video) {
-    this.#videos.push(video);
+export class DatabaseMemory {
+  #characters = new Map();
+
+  list(search) {
+    return Array.from(this.#characters.entries())
+      .map((charactersArray) => {
+        const id = charactersArray[0];
+        const data = charactersArray[1];
+        return {
+          id,
+          ...data,
+        };
+      })
+      .filter((character) => {
+        if (search) {
+          return character.name.includes(search);
+        }
+        return true;
+      });
   }
 
-  update(id, video) {
-    this.#videos.push(video);
+  create(character) {
+    const characterId = randomUUID();
+    this.#characters.set(characterId, character);
+  }
+
+  update(id, character) {
+    this.#characters.set(id, character);
+  }
+
+  delete(id) {
+    this.#characters.delete(id);
   }
 }
--- server.js
+++ server.js
@@ -1,11 +1,51 @@
 import { fastify } from "fastify";
+import { DatabaseMemory } from "./databaseMemory.js";
 
 const server = fastify();
 
-server.get('/', () => {
-  return 'Hello world'
-})
+const database = new DatabaseMemory();
+
+server.post("/characters", (request, reply) => {
+  const body = request.body;
+
+  database.create({
+    name: body.name,
+    level: body.level,
+    class: body.class,
+  });
+
+  console.log(database.list());
+
+  return reply.status(201).send();
+});
+
+server.get("/characters", (request, reply) => {
+  const search = request.query.search;
+  const data = database.list(search);
+  return data;
+});
+
+server.put("/characters/:id", (request, reply) => {
+  const characterId = request.params.id;
+  const body = request.body;
+
+  database.update(characterId, {
+    name: body.name,
+    level: body.level,
+    class: body.class,
+  });
+
+  return reply.status(204).send();
+});
+
+server.delete("/characters/:id", (request, reply) => {
+  const characterId = request.params.id;
+
+  database.delete(characterId);
+
+  return reply.status(204).send();
+});
 
 server.listen({
-  port: 3333
-})
+  port: 3333,
+});
