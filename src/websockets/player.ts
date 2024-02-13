2bc1ba890f81d5e42c57a0b758f985bb56c7e62f
alexanderJr <alexmarcjr21@gmail.com>
2024-02-13 16:36:21 +0000

    add setInterval as a workaround pooling on app

--- src/websockets/player.ts
+++ src/websockets/player.ts
@@ -1,14 +1,28 @@
 import { FastifyInstance } from "fastify";
 import { CharacterModel } from "../models/characterModel.ts";
-import { mongoClient } from "../config/db.ts";
 
 const database = new CharacterModel();
 
 export const websocketController = (server: FastifyInstance) => {
   server.io.on("connection", (socket: any) => {
-    // todo: ensure that each socket adds one listener at this event
+    let intervalId: NodeJS.Timeout;
+
     socket.on("fetch_character_data", async (email: string) => {
-      const playerData: [] = await database.list(email, "ws");
+      intervalId = setInterval(async () => {
+        const playerData = await database.list(email, "ws");
+        console.info(playerData);
+
+        if (playerData.length <= 0) {
+          socket.emit("character_data", {
+            error: "You don't have characters yet, create one first!",
+          });
+        } else {
+          socket.emit("character_data", { data: playerData });
+        }
+      }, 5000); // Fetch data every 5 seconds
+
+      // Initial fetch when the socket connects
+      const playerData = await database.list(email, "ws");
       console.info(playerData);
 
       if (playerData.length <= 0) {
@@ -20,23 +34,9 @@ export const websocketController = (server: FastifyInstance) => {
       }
     });
 
-    // const db = mongoClient.db("Player");
-    // const changeStream = db.collection("Player").watch();
-
-    // // Handle change events
-    // // changeStream.on("change", (next) => {
-    // //   // Process only update events, ignore insert events
-    // //   if (next.operationType === "update") {
-    // //     console.log("Character updated:", next.updateDescription.updatedFields);
-    // //     socket.emit("character_updated", {
-    // //       updatedFields: next.updateDescription.updatedFields,
-    // //     });
-    // //   }
-    // // });
-
-    // Clean up the change stream on socket disconnect
+    // Clean up the interval on socket disconnect
     socket.on("disconnect", () => {
-      // changeStream.close();
+      clearInterval(intervalId);
       console.log("Socket disconnected!", socket.id);
     });
   });
